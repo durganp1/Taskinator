3c4d6b8bd1a5da9135c17ca7c6e87c4a6650c32b
durganp1 <durganp1@gmail.com>
2021-01-04 11:13:54 -0600

    added css to dragable items

--- assets/js/script.js
+++ assets/js/script.js
@@ -200,6 +200,7 @@ var dropZoneDragHandler = function(event) {
     var taskListEl = event.target.closest('.task-list');
     if (taskListEl) {
         event.preventDefault();
+        taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
     }
 };
 
@@ -219,8 +220,16 @@ var dropTaskHandler = function(event) {
         statusSelectEl.selectedIndex = 2;
     }
 
+    dropZoneEl.removeAttribute('style');
     dropZoneEl.appendChild(draggableElement);
 };
+
+var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest('.task-list');
+    if (taskListEl) {
+        taskListEl.removeAttribute('style');
+    }
+}
 
 formEl.addEventListener('submit', taskFormHandler);
 pageContentEl.addEventListener('click', taskButtonHandler);
@@ -228,3 +237,4 @@ pageContentEl.addEventListener('change', taskStatusChangeHandler);
 pageContentEl.addEventListener('dragstart', dragTaskHandler);
 pageContentEl.addEventListener('dragover', dropZoneDragHandler);
 pageContentEl.addEventListener('drop', dropTaskHandler);
+pageContentEl.addEventListener('dragleave', dragLeaveHandler);
